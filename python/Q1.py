3512dfe6e80ef7682429f649e67b793bbf6e28c3
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-05-17 22:42:48 +0530

    practice questions

--- python/Q1.py
+++
@@ -1,2 +0,0 @@
-def camel_to_snake(text):
-  return re.sub(r'(?aZ)(?=[A-Z]|[0-9]|\W)', r'_\1', text).lower()
--- python/Q2.py
+++
@@ -1,9 +0,0 @@
-list = [3, 0, 1, 4, 6, 2, 7, 10]
-n = 10
-result = []
-
-for i in range(n + 1):
-    if i not in list:
-        result.append(i)
-
-print(result)
